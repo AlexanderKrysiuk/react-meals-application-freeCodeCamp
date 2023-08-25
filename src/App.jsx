84262bcb59b7b3cd013ef3863d11bb294d90dc06
Macbook Alexander <alex@macbook-pro-macbook.home>
2023-08-25 17:00:17 +0200

    Modal Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import './App.css'
+import { useGlobalContext } from './context'
 import Favourites from './components/Favorites'
 import Meals from './components/Meals'
 import Modal from './components/Modal'
 import Search from './components/Search'
 export default function App(){
+  const {showModal} = useGlobalContext()
   return(
     <main>
       <Search/>
       {/*<Favourites/>*/}
       <Meals/>
-      {/*<Modal/>*/}
+      {showModal && <Modal/>}
     </main>
   )
 }
--- src/context.jsx
+++ src/context.jsx
@@ -1,5 +1,6 @@
 import React, {useContext, useEffect, useState} from "react"
 import axios, { all } from "axios"
+import Favorites from "./components/Favorites"
 const AppContext = React.createContext()
 const allMealsUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s="
 const randomMealUrl = "https://www.themealdb.com/api/json/v1/1/random.php"
@@ -7,6 +8,8 @@ const AppProvider = ({children}) => {
     const [loading, setLoading] = useState(false)
     const [meals, setMeals] = useState([])
     const [searchTerm, setSearchTerm] = useState('')
+    const [showModal, setShowModal] = useState(false)
+    const [selectedMeal, setSelectedMeatl] = useState(null)
     const fetchMeals = async(url) => {
         setLoading(true)
         try{
@@ -24,6 +27,15 @@ const AppProvider = ({children}) => {
     const fetchRandomMeal = () => {
         fetchMeals(randomMealUrl)
     }
+    const selectMeal = (idMeal, favouriteMeal) => {
+        let meal;
+        meal = meals.find((meal)=>meal.idMeal === idMeal)
+        setSelectedMeatl(meal)
+        setShowModal(true);
+    }
+    const closeModal = () =>{
+        setShowModal(false)   
+    }
     useEffect(() => {
         fetchMeals(allMealsUrl)
     },[])
@@ -31,7 +43,7 @@ const AppProvider = ({children}) => {
         if(!searchTerm) return
         fetchMeals(`${allMealsUrl}${searchTerm}`)
     },[searchTerm])
-    return <AppContext.Provider value={{loading, meals, setSearchTerm, fetchRandomMeal}}>
+    return <AppContext.Provider value={{loading, meals, setSearchTerm, fetchRandomMeal, showModal, selectedMeal, selectMeal, closeModal}}>
         {children}
     </AppContext.Provider>
 }
